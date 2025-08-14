de79e1cb2f30ef48718d7a9cf2cf4dbb9435462a
NeelapuTarun <tarunmario87@gmail.com>
2025-08-14 19:20:37 +0530

    Initial commit with PerfectNo.java, README, and LICENSE

---
+++ PerfectNo.java
@@ -0,0 +1,36 @@
+// To check a perfect number in java...
+
+import java.util.Scanner;
+
+public class PerfectNo {
+    public static void main(String[] args){
+
+
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the number to check if it is a perfect number:");
+        int num = sc.nextInt();
+
+        int sum=0;
+
+        for (int i=1; i <= num/2; i++){
+            if  ( num % i ==0 ) {
+
+                sum =sum +i;
+
+            }
+            
+        }
+
+
+            if (sum == num){
+                System.out.println (num + "is a perfect number ");
+            }
+            else {
+                System.out.println (num + "is not a perfect number");
+            }
+
+            sc.close();
+        }
+    }
+    
+
