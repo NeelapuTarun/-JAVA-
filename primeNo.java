89b1fc4aecf998d47b4977b16ff2add37d8c732f
NeelapuTarun <tarunmario87@gmail.com>
2025-08-17 11:30:55 +0530

    Organize Leapyear.java into leap_year_checker folder

---
+++ primeNo.java
@@ -0,0 +1,17 @@
+// Prime Number Checker//
+
+import java.util.Scanner;
+
+public class primeNo {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+
+        System.out.println("Enter the number:");
+        int num = x.nextInt();
+
+        for(int i = 0; i <= num; i++) {
+            // Loop body is empty - you might want to put logic here
+        }
+        x.close();
+    }
+}
